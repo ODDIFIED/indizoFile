3db42d239afa2f30888148f772833e104a4b10f6
DAVID CHISOM <iamdheevee@gmail.com>
2022-09-18 23:02:26 +0100

    second commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded',function(event){
       // text finished, call callback if there is a callback function
       else if (typeof fnCallback == 'function') {
         // call callback after timeout
-        setTimeout(fnCallback, 700);
+        setTimeout(fnCallback, 0.4);
       }
     }
     // start a typewriter animation for a text in the dataText array
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded',function(event){
         // text exists! start typewriter animation
        typeWriter(dataText[i], 0, function(){
          // after callback (and whole text has been animated), start next text
-         StartTextAnimation(i + 2);
+         StartTextAnimation(i + 1);
        });
       }
     }
